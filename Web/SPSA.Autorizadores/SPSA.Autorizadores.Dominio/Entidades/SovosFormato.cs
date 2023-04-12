01619299ac2caa446a3af14f44591f44598c8309
Yovanny Zeballos - Inetum <yovanny.zeballos@inetum.com>
2023-04-12 14:57:41 -0500

    - Mantenimiento de locales

---
+++ Web/SPSA.Autorizadores/SPSA.Autorizadores.Dominio/Entidades/SovosFormato.cs
@@ -0,0 +1,9 @@
+﻿namespace SPSA.Autorizadores.Dominio.Entidades
+{
+    public class SovosFormato
+    {
+        public string CodEmpresa { get; set; }         
+        public string CodFormato { get; set; }         
+        public string Nombre { get; set; }         
+    }
+}
---
+++ Web/SPSA.Autorizadores/SPSA.Autorizadores.Infraestructura/Repositorio/RepositorioSovosFormato.cs
@@ -0,0 +1,64 @@
+﻿using SPSA.Autorizadores.Dominio.Contrato.Repositorio;
+using SPSA.Autorizadores.Dominio.Entidades;
+using SPSA.Autorizadores.Infraestructura.Utiles;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.Threading.Tasks;
+
+namespace SPSA.Autorizadores.Infraestructura.Repositorio
+{
+    public class RepositorioSovosFormato : CadenasConexion, IRepositorioSovosFormato
+    {
+        private readonly int _commandTimeout;
+
+        public RepositorioSovosFormato()
+        {
+            _commandTimeout = Convert.ToInt32(ConfigurationManager.AppSettings["CommandTimeout"]);
+        }
+
+        public async Task<List<SovosFormato>> LocListar(string codEmpresa)
+        {
+            var formatos = new List<SovosFormato>();
+            using (SqlConnection connection = new SqlConnection(CadenaConexionCarteleria))
+            {
+                var command = new SqlCommand("SP_LOC_LIST_FORMATO", connection)
+                {
+                    CommandType = CommandType.StoredProcedure,
+                    CommandTimeout = _commandTimeout
+                };
+
+                await command.Connection.OpenAsync();
+
+
+                command.Parameters.Add(new SqlParameter
+                {
+                    SqlDbType = SqlDbType.VarChar,
+                    Direction = ParameterDirection.Input,
+                    Size = 10,
+                    Value = codEmpresa,
+                    ParameterName = "@COD_EMPRESA"
+                });
+
+                var dr = await command.ExecuteReaderAsync(CommandBehavior.CloseConnection);
+
+                if (dr != null && dr.HasRows)
+                {
+                    while (await dr.ReadAsync())
+                    {
+                        formatos.Add(new SovosFormato
+                        {
+                            CodFormato = dr["COD_FORMATO"].ToString().Trim(),
+                            Nombre = dr["NOM_FORMATO"].ToString().Trim()
+                        });
+                    }
+                }
+                connection.Close();
+                connection.Dispose();
+                return formatos;
+            }
+        }
+    }
+}
