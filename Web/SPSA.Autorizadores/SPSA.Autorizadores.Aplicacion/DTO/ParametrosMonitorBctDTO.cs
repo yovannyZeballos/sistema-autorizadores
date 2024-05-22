9fb6e13ba2b1bfb66b7fd6c9449f85d0dcf97333
Waldir Gonzales <experis-waldir.gonzales@spsa.pe>
2024-05-22 14:54:11 -0500

    Obtener y setear parametros de estado conexion y fecha negocio

--- Web/SPSA.Autorizadores/SPSA.Autorizadores.Aplicacion/DTO/ParametrosMonitorBctDTO.cs
+++ Web/SPSA.Autorizadores/SPSA.Autorizadores.Aplicacion/DTO/ParametrosMonitorBctDTO.cs
@@ -1,9 +1,13 @@
-﻿namespace SPSA.Autorizadores.Aplicacion.DTO
+﻿using System;
+
+namespace SPSA.Autorizadores.Aplicacion.DTO
 {
 	public class ParametrosMonitorBctDTO : RespuestaComunDTO
 	{
 		public int ToleranciaSegundos { get; set; }
 		public int ToleranciaCantidad { get; set; }
 		public string CodEmpresa { get; set; }
+		public string FechaNegocio { get; set; }
+		public string EstadoConexion { get; set; }
 	}
 }
