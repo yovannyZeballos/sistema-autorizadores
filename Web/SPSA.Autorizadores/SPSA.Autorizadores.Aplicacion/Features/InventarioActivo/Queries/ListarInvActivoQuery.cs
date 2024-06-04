72a8271b1cf8c18c4aeefa19456b85c3f9b847ba
Waldir Gonzales <experis-waldir.gonzales@spsa.pe>
2024-06-04 13:04:52 -0500

    Inv Activos - v1

---
+++ Web/SPSA.Autorizadores/SPSA.Autorizadores.Aplicacion/Features/InventarioActivo/Queries/ListarInvActivoQuery.cs
@@ -0,0 +1,64 @@
+﻿using AutoMapper;
+using MediatR;
+using SPSA.Autorizadores.Aplicacion.DTO;
+using SPSA.Autorizadores.Aplicacion.Logger;
+using SPSA.Autorizadores.Dominio.Contrato.Repositorio;
+using SPSA.Autorizadores.Infraestructura.Contexto;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Threading;
+using Serilog;
+using System.Linq;
+using System.Data.Entity;
+
+namespace SPSA.Autorizadores.Aplicacion.Features.InventarioActivo.Queries
+{
+    public class ListarInvActivoQuery : IRequest<GenericResponseDTO<List<ListarInvActivoDTO>>>
+    {
+        public string CodEmpresa { get; set; }
+        public string CodCadena { get; set; }
+        public string CodRegion { get; set; }
+        public string CodZona { get; set; }
+        public string CodLocal { get; set; }
+    }
+
+    public class ListarInvActivoaHandler : IRequestHandler<ListarInvActivoQuery, GenericResponseDTO<List<ListarInvActivoDTO>>>
+    {
+        private readonly IMapper _mapper;
+        private readonly IBCTContexto _contexto;
+        private readonly ILogger _logger;
+        public ListarInvActivoaHandler(IMapper mapper)
+        {
+            _mapper = mapper;
+            _contexto = new BCTContexto();
+            _logger = SerilogClass._log;
+        }
+
+        public async Task<GenericResponseDTO<List<ListarInvActivoDTO>>> Handle(ListarInvActivoQuery request, CancellationToken cancellationToken)
+        {
+            var response = new GenericResponseDTO<List<ListarInvActivoDTO>> { Ok = true, Data = new List<ListarInvActivoDTO>() };
+
+            try
+            {
+                var activos = await _contexto.RepositorioInventarioActivo.Obtener(x => x.CodEmpresa == request.CodEmpresa 
+                                                                                    && x.CodCadena == request.CodCadena 
+                                                                                    && x.CodRegion == request.CodRegion 
+                                                                                    && x.CodZona == request.CodZona
+                                                                                    && x.CodLocal == request.CodLocal)
+                                                                        .OrderBy(x => x.CodActivo)
+                                                                        .ToListAsync();
+                response.Data = _mapper.Map<List<ListarInvActivoDTO>>(activos);
+                response.Ok = true;
+                response.Mensaje = "Se ha generado la lista correctamente";
+            }
+            catch (Exception ex)
+            {
+                response.Ok = false;
+                response.Mensaje = ex.Message;
+                _logger.Error(ex, response.Mensaje);
+            }
+            return response;
+        }
+    }
+}
--- Web/SPSA.Autorizadores/SPSA.Autorizadores.Web/Content/ViewJs/AdministrarInvActivo.js
+++ Web/SPSA.Autorizadores/SPSA.Autorizadores.Web/Content/ViewJs/AdministrarInvActivo.js
@@ -1,10 +1,36 @@
-﻿var urlImportarInvActivo = baseUrl + 'Inventario/AdministrarInventario/ImportarExcelInventario';
+﻿var urlListarEmpresas = baseUrl + 'Maestros/MaeEmpresa/ListarEmpresa';
+var urlListarCadenas = baseUrl + 'Maestros/MaeCadena/ListarCadena';
+var urlListarRegiones = baseUrl + 'Maestros/MaeRegion/ListarRegion';
+var urlListarZonas = baseUrl + 'Maestros/MaeZona/ListarZona';
+var urlListarLocales = baseUrl + 'Maestros/MaeLocal/ListarLocal';
+var urlListarActivos = baseUrl + 'Inventario/InventarioActivo/ListarLocal';
+
+
+var urlImportarInvActivo = baseUrl + 'Inventario/InventarioActivo/ImportarExcelInventario';
 
 var urlDescargarPlantilla = baseUrl + 'Maestros/MaeTablas/DescargarPlantillas';
 
 var AdministrarInvActivo = function () {
 
     const eventos = function () {
+
+        $("#cboEmpresa").on("change", async function () {
+            await cargarComboCadenas();
+        });
+
+        $("#cboCadena").on("change", async function () {
+            await cargarComboRegiones();
+        });
+
+        $("#cboRegion").on("change", async function () {
+            await cargarComboZonas();
+        });
+
+        $("#cboZona").on("change", async function () {
+            await cargarComboLocales();
+        });
+
+
         $("#btnCargarArchivo").click(function () {
             $("#modalImportarInvActivo").modal('show');
         });
@@ -34,6 +60,277 @@ var AdministrarInvActivo = function () {
             }
         });
     }
+
+    const listarEmpresas = function () {
+        return new Promise((resolve, reject) => {
+
+            const request = {
+            };
+
+            $.ajax({
+                url: urlListarEmpresas,
+                type: "post",
+                data: { request },
+                success: function (response) {
+                    resolve(response)
+                },
+                error: function (jqXHR, textStatus, errorThrown) {
+                    reject(jqXHR.responseText)
+                }
+            });
+        });
+
+    }
+
+    const listarCadenas = function () {
+        return new Promise((resolve, reject) => {
+            const codEmpresa = $("#cboEmpresa").val();
+            if (!codEmpresa) return resolve();
+
+            const request = {
+                CodEmpresa: codEmpresa
+            };
+
+            $.ajax({
+                url: urlListarCadenas,
+                type: "post",
+                data: { request },
+                success: function (response) {
+                    resolve(response)
+                },
+                error: function (jqXHR, textStatus, errorThrown) {
+                    reject(jqXHR.responseText)
+                }
+            });
+        });
+    }
+
+    const listarRegiones = function () {
+        return new Promise((resolve, reject) => {
+            const codEmpresa = $("#cboEmpresa").val();
+            const codCadena = $("#cboCadena").val();
+            if (!codEmpresa) return resolve();
+
+            const request = {
+                CodEmpresa: codEmpresa,
+                CodCadena: codCadena,
+            };
+
+            $.ajax({
+                url: urlListarRegiones,
+                type: "post",
+                data: { request },
+                success: function (response) {
+                    resolve(response)
+                },
+                error: function (jqXHR, textStatus, errorThrown) {
+                    reject(jqXHR.responseText)
+                }
+            });
+        });
+    }
+
+    const listarZonas = function () {
+        return new Promise((resolve, reject) => {
+            const codEmpresa = $("#cboEmpresa").val();
+            const codCadena = $("#cboCadena").val();
+            const codRegion = $("#cboRegion").val();
+
+            if (!codEmpresa) return resolve();
+            if (!codCadena) return resolve();
+            if (!codRegion) return resolve();
+
+            const request = {
+                CodEmpresa: codEmpresa,
+                CodCadena: codCadena,
+                CodRegion: codRegion
+            };
+
+            $.ajax({
+                url: urlListarZonas,
+                type: "post",
+                data: { request },
+                success: function (response) {
+                    resolve(response)
+                },
+                error: function (jqXHR, textStatus, errorThrown) {
+                    reject(jqXHR.responseText)
+                }
+            });
+        });
+
+    }
+
+    const listarLocales = function () {
+        return new Promise((resolve, reject) => {
+            const codEmpresa = $("#cboEmpresa").val();
+            const codCadena = $("#cboCadena").val();
+            const codRegion = $("#cboRegion").val();
+            const codZona = $("#cboZona").val();
+
+            if (!codEmpresa) return resolve();
+            if (!codCadena) return resolve();
+            if (!codRegion) return resolve();
+            if (!codZona) return resolve();
+
+            const request = {
+                CodEmpresa: codEmpresa,
+                CodCadena: codCadena,
+                CodRegion: codRegion,
+                CodZona: codZona
+            };
+
+            $.ajax({
+                url: urlListarLocales,
+                type: "post",
+                data: { request },
+                success: function (response) {
+                    resolve(response)
+                },
+                error: function (jqXHR, textStatus, errorThrown) {
+                    reject(jqXHR.responseText)
+                }
+            });
+        });
+
+    }
+
+
+    const cargarComboEmpresa = async function () {
+
+        try {
+            const response = await listarEmpresas();
+
+            if (response.Ok) {
+                $('#cboEmpresa').empty().append('<option label="Seleccionar"></option>');
+                response.Data.map(empresa => {
+                    $('#cboEmpresa').append($('<option>', { value: empresa.CodEmpresa, text: empresa.NomEmpresa }));
+                });
+                //$('#cboEmpresa').val("001");
+            } else {
+                swal({
+                    text: response.Mensaje,
+                    icon: "error"
+                });
+                return;
+            }
+        } catch (error) {
+            swal({
+                text: error,
+                icon: "error"
+            });
+        }
+    }
+
+    const cargarComboCadenas = async function () {
+
+        try {
+            const response = await listarCadenas();
+
+            if (response === undefined) return;
+
+            if (response.Ok) {
+                $('#cboCadena').empty().append('<option label="Seleccionar"></option>');
+                $('#cboRegion').empty().append('<option label="Seleccionar"></option>');
+                $('#cboZona').empty().append('<option label="Seleccionar"></option>');
+                response.Data.map(cadena => {
+                    $('#cboCadena').append($('<option>', { value: cadena.CodCadena, text: cadena.NomCadena }));
+                });
+            } else {
+                swal({
+                    text: response.Mensaje,
+                    icon: "error"
+                });
+                return;
+            }
+        } catch (error) {
+            swal({
+                text: error,
+                icon: "error"
+            });
+        }
+    }
+
+    const cargarComboRegiones = async function () {
+
+        try {
+            const response = await listarRegiones();
+            if (response === undefined) return;
+            if (response.Ok) {
+                $('#cboRegion').empty().append('<option label="Seleccionar"></option>');
+                $('#cboZona').empty().append('<option label="Seleccionar"></option>');
+                response.Data.map(region => {
+                    $('#cboRegion').append($('<option>', { value: region.CodRegion, text: region.NomRegion }));
+                });
+            } else {
+                swal({
+                    text: response.Mensaje,
+                    icon: "error"
+                });
+                return;
+            }
+        } catch (error) {
+            swal({
+                text: error,
+                icon: "error"
+            });
+        }
+    }
+
+    const cargarComboZonas = async function () {
+
+        try {
+            const response = await listarZonas();
+
+            if (response === undefined) return;
+
+            if (response.Ok) {
+                $('#cboZona').empty().append('<option label="Seleccionar"></option>');
+                response.Data.map(region => {
+                    $('#cboZona').append($('<option>', { value: region.CodZona, text: region.NomZona }));
+                });
+            } else {
+                swal({
+                    text: response.Mensaje,
+                    icon: "error"
+                });
+                return;
+            }
+        } catch (error) {
+            swal({
+                text: error,
+                icon: "error"
+            });
+        }
+    }
+
+    const cargarComboLocales = async function () {
+
+        try {
+            const response = await listarLocales();
+
+            if (response === undefined) return;
+
+            if (response.Ok) {
+                $('#cboLocal').empty().append('<option label="Seleccionar"></option>');
+                response.Data.map(region => {
+                    $('#cboLocal').append($('<option>', { value: region.CodLocal, text: region.NomLocal }));
+                });
+            } else {
+                swal({
+                    text: response.Mensaje,
+                    icon: "error"
+                });
+                return;
+            }
+        } catch (error) {
+            swal({
+                text: error,
+                icon: "error"
+            });
+        }
+    }
+
 
     const importarExcelInvActivo = function () {
         var archivo = document.getElementById('archivoExcelInvActivo').files[0];
@@ -109,8 +406,9 @@ var AdministrarInvActivo = function () {
 
     return {
         init: function () {
-            checkSession(function () {
+            checkSession(async function () {
                 eventos();
+                await cargarComboEmpresa();
                 //showLoading();
                 //closeLoading();
             });
