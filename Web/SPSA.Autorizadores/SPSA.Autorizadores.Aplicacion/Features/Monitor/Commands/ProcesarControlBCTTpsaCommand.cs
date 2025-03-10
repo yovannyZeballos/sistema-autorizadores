f233903cd1a1f0c51d057c6d37ba1d9cae348e22
wally_ <waldir.gonzales@gmail.com>
2025-03-10 10:18:37 -0500

    Correcion lista de locales BCT TP

--- Web/SPSA.Autorizadores/SPSA.Autorizadores.Aplicacion/Features/Monitor/Commands/ProcesarControlBCTTpsaCommand.cs
+++ Web/SPSA.Autorizadores/SPSA.Autorizadores.Aplicacion/Features/Monitor/Commands/ProcesarControlBCTTpsaCommand.cs
@@ -42,7 +42,7 @@ namespace SPSA.Autorizadores.Aplicacion.Features.Monitor.Commands
 
 				var horariosCT2 = await _repositorioMonitorControlBCT.ObtenerHorarioSucursalCT2Tpsa(fechaStr, request.CodSucursal);
 				//var horariosBCT = await _repositorioMonitorControlBCT.ObtenerHorarioSucursalBCTTpsa(fechaStr);
-                var horariosBCT = await _repositorioMonitorControlBCT.ObtenerHorarioSucursalBCT(fecha.ToString("dd/MM/yyyy"), request.CodSucursal);
+                var horariosBCT = await _repositorioMonitorControlBCT.ObtenerHorarioSucursalBCTTpsa(fecha.ToString("dd/MM/yyyy"), request.CodSucursal);
 
                 respuesta.Data = (from a in horariosCT2
 								  join b in horariosBCT on a.CodSucursal equals b.CodSucursal
